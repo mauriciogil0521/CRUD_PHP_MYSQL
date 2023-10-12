e850c0673948256393338d0d84343420e76d49c1
mauricio gil <67571028+mauriciogil0521@users.noreply.github.com>
2023-10-12 14:32:25 -0500

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MYSQL CRUD</title>
+    <!---BOOTSTRAP 4 LINK CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!--FONT AWESOME 5-->
+    <script src="https://kit.fontawesome.com/ca0fb6fc54.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand"> PHP MYSQL CRUD</a>
+    </div>    
+
+</nav>
